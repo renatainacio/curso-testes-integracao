8bc383938f5e31ea4948078b84a65762f3f1b7ea
Renata Inacio <revazgauska@gmail.com>
2023-09-26 10:34:31 -0300

    fix ex aula 03

--- aula-03/tests/app.test.ts
+++ aula-03/tests/app.test.ts
@@ -1,31 +1,61 @@
+import calculator from "calculator";
+
 describe("testes de soma", () => {
     it("should return the sum of two values", () => {
-        expect(0 + 0).toBe(0);
-        expect(1 + 0).toBe(1);
-        expect(1 + 1).toBe(2);
+        let sum: number;
+
+        sum = calculator.sum(0, 0);
+        expect(sum).toBe(0);
+
+        sum = calculator.sum(1, 0);
+        expect(sum).toBe(1);
+
+        sum = calculator.sum(1, 1);
+        expect(sum).toBe(2);
     });
 });
 
 describe("testes de subtração", () => {
     it("should return the sum of two values", () => {
-        expect(0 - 0).toBe(0);
-        expect(1 - 0).toBe(1);
-        expect(3 - 1).toBe(2);
+        let sub: number;
+
+        sub = calculator.sub(0, 0);
+        expect(sub).toBe(0);
+
+        sub = calculator.sub(1, 0);
+        expect(sub).toBe(1);
+    
+        sub = calculator.sub(3, 1);
+        expect(sub).toBe(2);
     });
 });
 
 describe("testes de multiplicação", () => {
     it("should return the sum of two values", () => {
-        expect(59 * 0).toBe(0);
-        expect(1 * 123).toBe(123);
-        expect(5 * 12).toBe(60);
+        let mult;
+
+        mult = calculator.mul(59, 0);
+        expect(mult).toBe(0);
+
+        mult = calculator.mul(123, 1);
+        expect(mult).toBe(123);
+
+        mult = calculator.mul(12, 5);
+        expect(mult).toBe(60);
     });
 });
 
 describe("testes de divisão", () => {
     it("should return the sum of two values", () => {
-        expect(320 / 10).toBe(32);
-        expect(42 / 7).toBe(6);
-        expect(15 / 1).toBe(15);
+        let div;
+
+        div = calculator.div(320, 10);
+        expect(div).toBe(32);
+
+        div = calculator.div(42, 7);
+        expect(div).toBe(6);
+
+        div = calculator.div(15, 1);
+        expect(div).toBe(15);
     });
 });
