542009f154c38d1717af8442006afe752a20ea5c
Renata Inacio <revazgauska@gmail.com>
2023-09-27 20:27:07 -0300

    ex aula 10

--- aula-06/src/app.ts
+++ aula-06/src/app.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/health", (req: Request, res: Response) => {
   res.status(200).send("OK!");
 });
